3ad7a7ee6282eb5da5bf9b1d228286b41c1a1938
Pidbid <35859787+Pidbid@users.noreply.github.com>
2019-11-29 13:58:52 +0800

    Add files via upload

---
+++ components/QS-tabs/QS-tabs.vue
@@ -0,0 +1,449 @@
+<template>
+	<view class="QS-tabs" :style="{
+	'z-index': zIndex,
+	'font-size': getFontSize + 'rpx',
+	'background-color': getBgColor,
+	'transition-duration': getDuration + 's'
+	}">
+		<scroll-view scroll-x class="QS-tabs-scroll" :scroll-left="left" scroll-with-animation :style="{ 
+			'z-index': (Number(zIndex) + 1)
+		}">
+			<view class="QS-tabs-scroll-box">
+				<view class="QS-tabs-scroll-item" :style="{
+				'height': getHeight + 'rpx', 
+				'line-height': getHeight + 'rpx',
+				'width': getWidth + 'rpx',
+				'color': index===getCurrent?getActiveColor:getDefaultColor,
+				'transition-duration': getDuration + 's',
+				'z-index': (Number(zIndex) + 2)
+			}"
+				 v-for="(item, index) in getTabs" :key="index" @tap="emit(index)">
+					<!-- line1 -->
+					<view v-if="animationMode==='line1'" class="boxStyle" :style="getDurationStyle +( index===getCurrent?getActiveStyle:getDefaultStyle)"></view>
+					{{item.name || item}}
+				</view>
+				<!-- itemBackground -->
+				<view v-if="hasItemBackground" class="itemBackgroundBox" :style="getItemBackgroundBoxStyle">
+					<view class="itemBackground" :style="getItemBackgroundStyle" />
+				</view>
+				<!-- line2 -->
+				<view v-if="animationMode==='line2'" class="boxStyle2" :style="getLinezIndex + getDurationStyle + getBoxStyle2" />
+				<view v-if="animationMode==='line3'" class="boxStyle2" :style="getLinezIndex + getBoxStyle3" />
+
+			</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	const {
+		windowWidth
+	} = uni.getSystemInfoSync();
+	export default {
+		props: {
+			tabs: { //需循环的标签列表
+				type: Array,
+				default () {
+					return [];
+				}
+			},
+			current: { //当前所在滑块的 index
+				type: Number,
+				default: 0
+			},
+			height: { //QS-tabs的高度和行高
+				type: [String, Number],
+				default: 80
+			},
+			width: { //单个tab的宽度
+				type: [String, Number],
+				default: 250
+			},
+			fontSize: { //字体大小
+				type: [String, Number],
+				default: 30
+			},
+			duration: { //过渡动画时长, 单位 s
+				type: [String, Number],
+				default: .5
+			},
+			activeColor: { //选中项的主题颜色
+				type: String,
+				default: '#33cc33'
+			},
+			defaultColor: { //未选中项的主题颜色
+				type: String,
+				default: '#888'
+			},
+			animationLineWidth: { //动画线条的宽度
+				type: [String, Number],
+				default: 20
+			},
+			line2Style: { //line2线条的样式
+				type: String,
+				default: 'height: 8rpx;border-radius: 4rpx;'
+			},
+			animationMode: { //动画类型
+				type: String,
+				default: 'line1'
+			},
+			autoCenter: { //是否自动滚动至中心目标
+				type: Boolean,
+				default: true
+			},
+			autoCenterMode: { //滚动至中心目标类型
+				type: String,
+				default: 'component'
+			},
+			activeStyle: { //line1模式选中项的样式
+				type: String,
+				default: 'bottom:0;left:50%;transform: translate(-50%,-100%);height: 8rpx;border-radius:4rpx;'
+			},
+			defaultStyle: { //line1模式未选中项的样式
+				type: String,
+				default: 'bottom:0;left:50%;transform: translate(-50%,-100%);height: 8rpx;border-radius:4rpx;'
+			},
+			backgroundColor: { //统一背景颜色
+				type: String,
+				default: 'rgba(255,255,255,0)'
+			},
+			hasItemBackground: { //是否开启背景追光
+				type: Boolean,
+				default: false
+			},
+			itemBackgroundColor: { //统一追光背景颜色
+				type: String,
+				default: 'rgba(255,255,255,0)'
+			},
+			itemBackgroundStyle: { //追光样式
+				type: String,
+				default: ''
+			},
+			zIndex: { //css的z-index属性值
+				type: [String, Number],
+				default: 99
+			},
+			swiperWidth: {
+				type: [String, Number],
+				default: 750
+			}
+		},
+		computed: {
+			getCurrent() {
+				const current = Number(this.current);
+				if (current > (this.getTabs.length - 1)) {
+					return (this.getTabs.length - 1)
+				}
+				return current;
+			},
+			getTabs() {
+				return this.tabs;
+			},
+			getHeight() {
+				return Number(this.height);
+			},
+			getWidth() {
+				return Number(this.width);
+			},
+			getFontSize() {
+				return this.fontSize;
+			},
+			getDuration() {
+				return Number(this.duration);
+			},
+			getBgColor() {
+				const defaultColor = this.backgroundColor || 'rgba(255,255,255,0)';
+				if (this.getTabs[this.getCurrent] instanceof Object) {
+					return this.getTabs[this.getCurrent].backgroundColor || defaultColor;
+				} else {
+					return defaultColor;
+				}
+			},
+			getItemBackgroundColor() {
+				const defaultColor = this.itemBackgroundColor || 'rgba(255,255,255,0)';
+				if (this.getTabs[this.getCurrent] instanceof Object) {
+					return this.getTabs[this.getCurrent].itemBackgroundColor || defaultColor;
+				} else {
+					return defaultColor;
+				}
+			},
+			getDurationStyle() {
+				return `transition-duration: ${this.getDuration}s;`
+			},
+			getActiveColor() {
+				let activeColor;
+				if (this.getTabs[this.getCurrent] instanceof Object) {
+					if (this.getTabs[this.getCurrent].activeColor) {
+						activeColor = this.getTabs[this.getCurrent].activeColor;
+					} else {
+						activeColor = this.activeColor;
+					}
+				} else {
+					activeColor = this.activeColor;
+				}
+				return activeColor;
+			},
+			getDefaultColor() {
+				let defaultColor;
+				if (this.getTabs[this.getCurrent] instanceof Object) {
+					if (this.getTabs[this.getCurrent].defaultColor) {
+						defaultColor = this.getTabs[this.getCurrent].defaultColor;
+					} else {
+						defaultColor = this.defaultColor;
+					}
+				} else {
+					defaultColor = this.defaultColor;
+				}
+				return defaultColor;
+			},
+			getActiveStyle() {
+				return `width:${this.animationLineWidth}%;background-color:${this.getActiveColor};${this.activeStyle};`;
+			},
+			getDefaultStyle() {
+				return `width:0;background-color:${this.getActiveColor};${this.defaultStyle};`;
+			},
+			getLinezIndexNum() {
+				return Number(this.zIndex) + 2;
+			},
+			getLinezIndex() {
+				return `z-index: ${this.getLinezIndexNum};`;
+			},
+			getBoxStyle2() {
+				const w = this.pxWidth;
+				const x = w * (this.getCurrent + 1) - w / 2 - (this.lW / 2);
+				return `transform:translate(${x}px, -100%);width:${this.lW}px;background-color: ${this.getActiveColor};${this.line2Style};`;
+			},
+			getItemBackgroundBoxStyle() {
+				return `height: ${this.getHeight}rpx;
+						width: ${this.getWidth}rpx;
+						z-index: ${(Number(this.zIndex) + 1)};
+						transition-duration: ${this.getDuration}s;
+						transform: translateX(${Number(this.width)*(this.getCurrent)}rpx);`
+			},
+			getItemBackgroundStyle() {
+				return `transition-duration: ${this.getDuration}s;
+						background-color: ${this.getItemBackgroundColor};
+						box-shadow: 0 0 5rpx 5rpx ${this.getItemBackgroundColor};
+						${this.itemBackgroundStyle};`
+			},
+			getFinishDx() {
+				const w = this.pxWidth;
+				const a = w * (Number(this.animationFinishCurrent) + 1);
+				const b = w / 2;
+				const c = this.lW / 2;
+				const x = a - b - c;
+				return x;
+			},
+			getDxNum() {
+				const b = this.dx / this.sW;
+				const a = b * this.pxWidth;
+				return a;
+			},
+			getBoxStyle3() {
+				const x = this.getFinishDx + this.getDxNum;
+				return `transform:translate(${x}px, -100%);width:${this.lW}px;background-color: ${this.getActiveColor};${this.line2Style};`;
+			}
+		},
+		watch: {
+			current(n, o) {
+				this.change(n);
+			}
+		},
+		data() {
+			return {
+				left: 0,
+				line2Width: Number(this.animationLineWidth),
+				setTimeoutFc: null,
+				componentsWidth: 0,
+				animationFinishCurrent: this.current,
+				dx: 0,
+				pxWidth: 0,
+				lW: 0,
+				sW: 0
+			}
+		},
+		// #ifndef H5
+		onReady() {
+			this.getQuery(() => {
+				this.countScrollX();
+			});
+			this.countPx();
+			this.getItemWidth();
+		},
+		// #endif
+		// #ifdef H5
+		mounted() {
+			this.getQuery(() => {
+				this.countScrollX();
+			});
+			this.countPx();
+			this.getItemWidth();
+		},
+		// #endif
+		methods: {
+			countPx() {
+				const w = uni.upx2px(Number(this.width));
+				this.pxWidth = w;
+				this.lW = w * (Number(this.animationLineWidth) / 100);
+				this.sW = uni.upx2px(Number(this.swiperWidth));
+			},
+			emit(index) {
+				this.$emit('change', index);
+			},
+			change() {
+				this.countScrollX();
+				if (this.animationMode === 'line2') {
+					this.line2Width = 2;
+					if (this.setTimeoutFc) clearTimeout(this.setTimeoutFc);
+					this.setTimeoutFc = setTimeout(() => {
+						this.line2Width = this.animationLineWidth;
+					}, this.getDuration * 1000 * 3 / 5);
+				}
+			},
+			getItemWidth() {
+					let view = uni.createSelectorQuery().in(this).select('.QS-tabs-scroll-item');
+					view.fields({
+						size: true
+					}, data => {
+						console.log(JSON.stringify(data));
+					}).exec();
+			},
+			getQuery(cb) {
+				try {
+					let view = uni.createSelectorQuery().in(this).select('.QS-tabs');
+					view.fields({
+						size: true
+					}, data => {
+						if (data) {
+							this.componentsWidth = data.width;
+							if (cb && typeof cb === 'function') cb(data);
+						} else {
+							this.retryGetQuery(cb);
+						}
+					}).exec();
+				} catch (e) {
+					//TODO handle the exception
+					this.componentsWidth = windowWidth;
+				}
+			},
+			retryGetQuery(cb) {
+				try {
+					let view = uni.createSelectorQuery().select('.QS-tabs');
+					view.fields({
+						size: true
+					}, data => {
+						if (data) {
+							this.componentsWidth = data.width;
+						} else {
+							this.componentsWidth = windowWidth;
+						}
+						if (cb && typeof cb === 'function') cb(data);
+					}).exec();
+				} catch (e) {
+					//TODO handle the exception
+					this.componentsWidth = windowWidth;
+				}
+			},
+			countScrollX() {
+				if (this.autoCenter) {
+					const width = Number(this.width);
+					const itemWidth = width / 750 * windowWidth;
+					let left = itemWidth * (this.getCurrent + 1) - itemWidth / 2;
+					let fatherWidth;
+					if (this.autoCenterMode === 'window') {
+						fatherWidth = windowWidth;
+					} else {
+						fatherWidth = this.componentsWidth;
+					}
+					this.left = left - fatherWidth / 2;
+				}
+			},
+			setDx(dx) {
+				this.dx = dx;
+			},
+			setFinishCurrent(current) {
+				this.dx = 0;
+				this.animationFinishCurrent = current;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	view,
+	scroll-view {
+		box-sizing: border-box;
+	}
+
+	.QS-tabs {
+		width: 100%;
+		transition-property: background-color, color;
+	}
+	
+	.QS-tabs::-webkit-scrollbar {
+        display: none;  
+        width: 0 !important;  
+        height: 0 !important;  
+        -webkit-appearance: none;  
+        background: transparent;  
+    }
+
+	.QS-tabs-scroll {
+		width: 100%;
+		white-space: nowrap;
+		position: relative;
+	}
+	
+	.QS-tabs-scroll-box{
+		position: relative;
+		display: flex;
+		white-space: nowrap !important;
+		display: block !important;
+	}
+
+	.QS-tabs-scroll-item {
+		position: relative;
+		display: inline-block;
+		text-align: center;
+		transition-property: background-color, color;
+		padding: 0 20rpx;
+	}
+
+	.content {
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+	}
+
+	.boxStyle {
+		pointer-events: none;
+		position: absolute;
+		transition-property: all;
+	}
+
+	.boxStyle2 {
+		pointer-events: none;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		transition-property: all;
+	}
+
+	.itemBackgroundBox {
+		pointer-events: none;
+		position: absolute;
+		top: 0;
+		left: 0;
+		transition-property: transform, background-color;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.itemBackground {
+		height: 100%;
+		width: 100%;
+		transition-property: all;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,376 @@
+<template>
+	<view>
+		<view>
+			<uni-notice-bar show-close="true" show-icon="true" background-color="#ffffff" text="视频均来自网络,请勿相信视频内广告!"></uni-notice-bar>
+		</view>
+		<view>
+			<uni-fab :pattern="pattern" :content="content" :horizontal="horizontal" :vertical="vertical" :direction="direction"
+			 @trigger="trigger"></uni-fab>
+		</view>
+		<view><video :src="newmovie" :autoplay="autoplay" id="video1" controls poster="this is a post image link"></video></view>
+		<view class="box">
+			<QSTabs ref="tabs" :tabs="tabs" animationMode="line3" :current="current" @change="change" activeColor="#fcd217"
+			 swiperWidth="750">
+			</QSTabs>
+		</view>
+		<swiper class="swipClass" :current="current" @transition="transition" @animationfinish="animationfinish">
+			<swiper-item class="swiper-item" v-for="(item, index) in tabs" :key="index">
+				<view class="jishu" v-for="(msg,ii) in allMsg[index]">
+					<uni-tag type="warning" inverted="true" :text="msg['title']" @click='playVideo(index,ii)'></uni-tag>
+				</view>
+			</swiper-item>
+		</swiper>
+		<uni-popup ref="showshare" type="bottom">
+			<view class="uni-share">
+				<text class="uni-share-title">快速打开</text>
+				<view class="uni-share-content">
+					<view v-for="(item, index) in bottomData" :key="index" class="uni-share-content-box">
+						<view @click="fastTo(item.name)">
+							<view class="uni-share-content-image">
+								<image :src="item.icon" class="content-image" mode="widthFix" />
+							</view>
+							<text class="uni-share-content-text">{{ item.text }}</text>
+						</view>
+					</view>
+				</view>
+				<text class="uni-share-btn" @click="cancel('share')">取消</text>
+			</view>
+		</uni-popup>
+	</view>
+</template>
+
+<script>
+	import QSTabs from '@/components/QS-tabs/QS-tabs.vue';
+	import uniNoticeBar from "@/components/uni-notice-bar/uni-notice-bar.vue";
+	import uniFab from '@/components/uni-fab/uni-fab.vue';
+	import uniTag from "@/components/uni-tag/uni-tag.vue";
+	import sortData from '../../data/sortData.js'
+	import uniPopup from '@/components/uni-popup/uni-popup.vue'
+	export default {
+		components: {
+			QSTabs,
+			uniNoticeBar,
+			uniFab,
+			uniTag,
+			uniPopup
+		},
+		data() {
+			return {
+				newmovie: "http://cn7.7639616.com/hls/20191117/74b1a65f695497ec585522a5128c7da3/1573976747/index.m3u8",
+				playMsg:[],
+				tabs: [],
+				current: 0,
+				tabsHeight: 0,
+				dx: 0,
+				zjMsg: [],
+				allMsg: [],
+				horizontal: 'left',
+				vertical: 'bottom',
+				direction: 'vertical',
+				inverted: 'true',
+				autoplay: false,
+				sgetMsg: [],
+				huancunData: sortData,
+				list: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+				pattern: {
+					color: '#7A7E83',
+					backgroundColor: '#fff',
+					selectedColor: '#f5a623',
+					buttonColor: "#f5a623"
+				},
+				content: [{
+						iconPath: '/static/go_3.png',
+						selectedIconPath: '/static/go_3.png',
+						text: '快速播放'
+					},
+					{
+						iconPath: '/static/go_2.png',
+						selectedIconPath: '/static/go_2.png',
+						text: '更新'
+					},
+					{
+						iconPath: '/static/go_1.png',
+						selectedIconPath: '/static/go_1.png',
+						text: '关于'
+					}
+				],
+				bottomData: [{
+						text: '阿拉巴斯坦',
+						icon: '../../static/fast/s6.png',
+						name: '1'
+					},
+					{
+						text: '司法岛战役228-325',
+						icon: '../../static/fast/s1.png',
+						name: '2'
+					}, {
+						text: '恐怖三帆船337-381',
+						icon: '../../static/fast/s4.png',
+						name: '3'
+					},
+					{
+						text:'推进城',
+						icon:'../../static/fast/s3.png',
+						name:'4'
+					},
+					{
+						text: '顶上战争459-489',
+						icon: '../../static/fast/s2.png',
+						name: '5'
+					}, {
+						text: '鱼人岛战役523-574',
+						icon: '../../static/fast/s5.png',
+						name: '6'
+					}
+				]
+			}
+		},
+		onLoad() {
+			/*此部分事从网络上获取播放列表
+			function qMsg() {
+				var a = []
+				uni.request({
+					url: 'json file link',
+					success: function(res) {
+						a = res.data
+					}
+				})
+				return a
+			}
+			var getMsg = []
+			try {
+				getMsg = uni.getStorageSync('allMsg')
+				var xin = qMsg()
+				if (getMsg != xin) {
+					console.log('检测到更新')
+					uni.setStorage({
+						key: 'allMsg',
+						data: xin
+					})
+				}
+			} catch (e) {
+				getMsg = qMsg()
+				console.log(123)
+			}
+			*/
+			var oldMsg = this.huancunData
+			var getMsg = oldMsg['data']
+			var getNew = getMsg['all'][0]['title'] //获取当前更新的最新集数
+			this.newmovie = getMsg['all'][0]['link']
+			var changdu = (getNew - getNew % 100) / 100
+			for (var i = 0; i <= changdu; i++) {
+				if (i == changdu) {
+					var title = i * 100 + 1 + '-' + getNew
+				} else {
+					var title = i * 100 + 1 + '-' + (i + 1) * 100
+				}
+				this.$set(this.tabs, i, String(title)) //实时修改数组
+			}
+			this.tabs.reverse() //翻转数组，将最新更新的排在前面
+			for (var i = 0; i <= changdu; i++) {
+				if (i == 0) {
+					for (var p = 0; p < getNew % 100; p++) {
+						const shipin = getMsg['all'][p]
+						this.$set(this.zjMsg, p, shipin)
+						this.$set(this.allMsg, i, this.zjMsg)
+					}
+				} else {
+					for (var q = 0; q <= 100; q++) {
+						if (q == 0) {
+							this.zjMsg = []
+						} else {
+							const yunsuan = 100 * i - 90 + q
+							const shipin = getMsg['all'][yunsuan]
+							this.zjMsg.push(shipin)
+							//this.$set(this.zjMsg,p,shipin)
+							this.$set(this.allMsg, i, this.zjMsg)
+						}
+					}
+				}
+			}
+		},
+		methods: {
+			change(index) {
+				this.current = index;
+			},
+			transition({
+				detail: {
+					dx
+				}
+			}) { //swiper-item位置改变时触发的事件
+				this.$refs.tabs.setDx(dx);
+			},
+			animationfinish({
+				detail: {
+					current
+				}
+			}) { //动画结束时触发的事件
+				this.$refs.tabs.setFinishCurrent(current);
+				this.current = current;
+			},
+			playVideo(da, xiao) {
+				this.newmovie = this.allMsg[da][xiao]['link']
+				var playJishu = this.allMsg[da][xiao]['title']
+				console.log(this.allMsg[da][xiao]['link'])
+				uni.showToast({
+					title: '你正在播放第' + playJishu + '集，请勿相信视频内广告！',
+					position: 'bottom',
+					success() {
+						//var mk = uni.createVideoContext('video1')
+						uni.pageScrollTo({
+							scrollTop: 0,
+							duration: 300
+						})
+					}
+				})
+
+			},
+			trigger(e) {
+				this.content[e.index].active = !e.item.active;
+				//console.log(e.index)
+				if (e.index == 0) {
+					this.$refs.showshare.open()
+				} else if (e.index == 1) {
+
+				} else {
+
+				}
+			},
+			setInverted() {
+				this.inverted = !this.inverted;
+			},
+			fastTo(a){
+				console.log(typeof(a))
+				var b = Number(a)
+				const c = uni.getStorageSync('allMsg')
+				console.log(c)
+				switch (b){
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	.body {
+		/*距离顶部范围应该在88-95范围内*/
+		padding-top: 90upx;
+		top: var(--status-bar-height);
+	}
+
+	.tanchu {
+		z-index: 999;
+	}
+
+	video {
+		width: 100%;
+		top: 0;
+		position: sticky;
+	}
+
+	.swipClass {
+		height: 900px;
+	}
+
+	.box {
+		width: 100%;
+		position: sticky;
+		top: 0;
+		z-index: 10;
+		background-color: white;
+	}
+
+	.jishu {
+		float: left;
+		width: 20%;
+		padding: 5px 10px 0px 10px;
+		box-sizing: border-box;
+		background-clip: content-box;
+	}
+
+	.anniu {}
+
+	/* 底部分享 */
+	.uni-share {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		flex-direction: column;
+		/* #endif */
+		background-color: #fff;
+		border-radius: 40rpx;
+	}
+
+	.uni-share-title {
+		line-height: 60rpx;
+		font-size: 30rpx;
+		padding: 15rpx 0;
+		text-align: center;
+	}
+
+	.uni-share-content {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		padding: 15px;
+	}
+
+	.uni-share-content-box {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		align-items: center;
+		width: 200rpx;
+	}
+
+	.uni-share-content-image {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		width: 75rpx;
+		height: 75rpx;
+		overflow: hidden;
+		border-radius: 10rpx;
+	}
+
+	.content-image {
+		width: 75rpx;
+		height: 75rpx;
+	}
+
+	.uni-share-content-text {
+		font-size: 28rpx;
+		color: #333;
+		padding-top: 5px;
+		padding-bottom: 10px;
+	}
+
+	.uni-share-btn {
+		height: 90rpx;
+		line-height: 90rpx;
+		font-size: 16px;
+		border-top-color: #f5f5f5;
+		border-top-width: 1px;
+		border-top-style: solid;
+		text-align: center;
+		color: #666;
+	}
+</style>
